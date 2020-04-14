cdcbfcdb6acd14c20f50d482ed590601c9724276
JackCallander <z5207123@cse.unsw.edu.au>
2020-04-14 19:12:42 +1000

    Added assets

---
+++ myCiphers/CipherTemplate.java
@@ -0,0 +1,8 @@
+public class CipherTemplate {
+    public static String encode(String message, String key, int value, char letter) {
+        return null;
+    }
+    public static String decode(String message, String key, int value, char letter) {
+        return null;
+    }
+}
---
+++ myCiphers/ParrotCipher.java
@@ -0,0 +1,8 @@
+public class ParrotCipher {
+    public static String encode(String message, String key, int value, char letter) {
+        return message;
+    }
+    public static String decode(String message, String key, int value, char letter) {
+        return message;
+    }
+}
